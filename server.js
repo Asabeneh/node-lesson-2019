9cf48951fcc9e8b9339f8cbc68dcf162a9cd55ca
Asabeneh <asabeneh@gmail.com>
2019-05-28 16:38:11 +0300

    getting req method, url and

--- server.js
+++ server.js
@@ -4,7 +4,14 @@ const PORT = process.env.PORT || 5000;
 
 http
   .createServer((req, res) => {
-    console.log('');
+    const method = req.method;
+    const url = req.url;
+    console.log('Method:', method, 'url: ', url);
+    // setting the response header, it takes status and mime type
+    const status = 200; // ok
+    const mimeType = { 'Content-Type': 'text/plain' }; // plain text
+    res.writeHead(status, mimeType);
+    res.end('Hello, World');
   })
   .listen(PORT);
 console.log(`Server is running at port ${PORT}...`);
