a060ca0c98927c21df314b309dcdfbc07c9bc33d
Asabeneh <asabeneh@gmail.com>
2019-05-28 16:25:10 +0300

    http core module added to create a server

--- server.js
+++ server.js
@@ -1,1 +1,11 @@
-console.log('Welcome to node.js, node loves you!');
+// node core modules
+const http = require('http');
+const PORT = process.env.PORT || 5000;
+
+http
+  .createServer((req, res) => {
+    console.log;
+  })
+  .listen(PORT);
+console.log(`Server is running at port ${PORT}`);
+
